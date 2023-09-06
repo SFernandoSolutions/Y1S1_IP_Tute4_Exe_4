5416e6d67194866b9ee14cb7924a46db1f8232b6
SFernando Solutions <143948014+SFernandoSolutions@users.noreply.github.com>
2023-09-06 11:27:33 +0530

    Update exe.c

--- exe.c
+++ exe.c
@@ -1,32 +1,37 @@
 #include <stdio.h>
 int main(void){
+//	create variables
 	double num1, num2, output;
 	char operator;
 	
+//	input operator
 	printf("Select the operator [+ : - : x : /] : ");
 	scanf("%c",&operator);
 	
+//	input 1 st number
 	printf("Enter your Number 1 : ");
 	scanf("%lf",&num1);
 	
+//	input 2 nd number
 	printf("Enter your Number 2 : ");
 	scanf("%lf",&num2);
 	
+//	switch operator
 	switch(operator){
 		case '+':
-			output=num1+num2;
+			output=num1+num2; //calculation
 			break;
 			
 		case '-':
-			output=num1-num2;
+			output=num1-num2; //calculation
 			break;
 			
 		case 'x':
-			output=num1*num2;
+			output=num1*num2; //calculation
 			break;
 			
 		case '/':
-			output=num1/num2;
+			output=num1/num2; //calculation
 			break;
 			
 		default:
@@ -34,6 +39,7 @@ int main(void){
 			break;
 	}
 	
+//		output
 		printf("Out put : %.2lf",output);
 	
 }
